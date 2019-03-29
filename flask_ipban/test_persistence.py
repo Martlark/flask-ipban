2b6ce722bc994831d7431dea1848882f7716d705
andrew rowe <andrew.rowe@playtech.com>
2019-03-29 16:33:31 +1100

    Fix persistence test.

--- flask_ipban/test_persistence.py
+++ flask_ipban/test_persistence.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 
-
 import os
 import tempfile
 
@@ -16,7 +15,7 @@ def hello_world(parameter=None):
     return page_text.format(parameter)
 
 
-tmp_file_name = os.path.join(tempfile.gettempdir(), 'blah.pickle')
+persist_dir = os.path.join(tempfile.gettempdir(), 'flask-ip-ban-testing')
 
 
 class IpBanPersistence1(unittest.TestCase):
@@ -24,13 +23,12 @@ class IpBanPersistence1(unittest.TestCase):
     def setUp(self):
         self.app = flask.Flask(__name__)
         self.ban_seconds = 2
-        self.ip_ban = IpBan(self.app, ban_seconds=self.ban_seconds, ban_count=5, persist=True,
-                            persist_file_name=tmp_file_name)
+        self.ip_ban = IpBan(self.app, ban_seconds=self.ban_seconds, ban_count=5, persist=True, record_dir=persist_dir)
         self.client = self.app.test_client()
 
         self.app.route('/')(hello_world)
 
-    def testPersistence1(self):
+    def runTest(self):
         self.ip_ban.block(['123.456.765.111'])
 
 
@@ -39,13 +37,12 @@ class IpBanPersistence2(unittest.TestCase):
     def setUp(self):
         self.app = flask.Flask(__name__)
         self.ban_seconds = 2
-        self.ip_ban = IpBan(self.app, ban_seconds=self.ban_seconds, ban_count=5, persist=True,
-                            persist_file_name=tmp_file_name)
+        self.ip_ban = IpBan(self.app, ban_seconds=self.ban_seconds, ban_count=5, persist=True, record_dir=persist_dir)
         self.client = self.app.test_client()
 
         self.app.route('/')(hello_world)
 
-    def testPersistence2(self):
+    def runTest(self):
         self.assertTrue('123.456.765.111' in self.ip_ban._ip_ban_list)
 
 
