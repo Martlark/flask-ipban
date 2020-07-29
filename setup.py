fdfdf519c117815ae2fabc773a72eeb5d80499b3
andrew rowe <mr_andrew_d_rowe@hotmail.com>
2020-07-29 10:10:14 +1000

    1.1.0 - Add cidr blocking.  Remove older python support.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from codecs import open
 from setuptools import setup
 
-VERSION = '1.0.13'
+VERSION = '1.0.14'
 LONG_DESCRIPTION = open('README.rst', 'r', encoding='utf-8').read()
 
 setup(
@@ -44,11 +44,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
 
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
